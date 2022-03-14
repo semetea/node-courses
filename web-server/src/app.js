4b2f90e0bd8e1bac44185815d4f45b5796f6e730
semetea <taewooocean@gmail.com>
2022-03-14 20:59:17 +0900

    static assest

---
+++ web-server/src/app.js
@@ -0,0 +1,31 @@
+const path = require('path')
+const express = require('express')
+
+console.log(__dirname)
+console.log(path.join(__dirname, '../public'))
+
+const app = express()
+const publicDirectoryPath = path.join(__dirname, '../public')
+
+app.use(express.static(publicDirectoryPath))
+
+// app.get('', (req, res)=> {
+//     res.send('<h1>Weather</h1>')
+// })
+
+
+
+app.get('/weather',(req, res)=> {
+    res.send({
+        forecast: 'Sunny',
+        location: 'Seoul'
+    })
+})
+
+// app.com
+// app.com/help
+// app.com/about
+
+app.listen(3000, () => {
+    console.log('Server is up on port 3000')
+})
