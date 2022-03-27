0b19537cd691b65e8dd5858df7d42ed63b8b82f0
semetea <taewooocean@gmail.com>
2022-03-27 19:51:24 +0900

    last

--- web-server/src/utils/forecast.js
+++ web-server/src/utils/forecast.js
@@ -1,6 +1,8 @@
 const request = require('postman-request')
 
 
+
+
 const forecast = (latitude, longtitude, callback) => {
     const url = "http://api.weatherstack.com/current?access_key=6c15c01c6e5fa31d683e3d1f359860ab&query=" + latitude + "," + longtitude
     
@@ -12,7 +14,7 @@ const forecast = (latitude, longtitude, callback) => {
             callback('Unable to find the location', undefined)
         }
         else {
-            callback(undefined, body.current.weather_descriptions[0] + '. It is currently ' + body.current.temperature + ' degrees out. It feels like ' + body.current.feelslike + ' degress out')
+            callback(undefined, body.current.weather_descriptions[0] + '. It is currently ' + body.current.temperature + ' degrees out. It feels like ' + body.current.feelslike + ' degress out. The humidity is ' + body.current.humidity + "%")
         }
     })
 }
