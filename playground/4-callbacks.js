e5a20e081f7fd7b33d68bc9a74011b481ba820ed
semetea <55260856+semetea@users.noreply.github.com>
2022-04-14 14:21:53 +0900

    callback

--- playground/4-callbacks.js
+++ playground/4-callbacks.js
@@ -1,41 +1,16 @@
-// setTimeout(()=> {
-//     console.log('Two seconds are up')
-// }, 2000)
-
-// const names = ['Terry', 'Jen', 'Jess']
-// const shortNames = names.filter((name)=> {
-//     return name.length <= 4
-// })
-
-const geocode = (address, callback) => {
-    setTimeout(()=>{
-        const data = {
-            latitude: 0,
-            longtitude: 0
-        }
-    
-        callback(data)
+const doWorkCallback = (callback) => {
+    setTimeout(() => {
+        // callback('This is my error!', undefined)
+        callback(undefined, [1, 4, 7])
     }, 2000)
 }
 
-geocode('Seoul',(data)=>{
-    console.log(data)
-    
+doWorkCallback((error, result) => {
+    if(error) {
+        return console.log(error)
+    }
+
+    console.log(result)
 })
-
-
-// setTimeout(()=> {
-//     console.log('2 seconds')
-// }, 2000)
-
-
-// const add = (a,b,callback) => {
-//     setTimeout(()=> {
-//         callback(a+b)
-//     }, 2000)
-// }
-// add(1, 4, (sum) => {
-//     console.log(sum) // Should print: 5
-// })
 
 
