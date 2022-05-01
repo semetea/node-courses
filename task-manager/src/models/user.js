6fa7f9502c81953630a90eff38ef393af5fa0dda
semetea <taewooocean@gmail.com>
2022-05-01 16:36:50 -0400

    CR operation

---
+++ task-manager/src/models/user.js
@@ -0,0 +1,43 @@
+const mongoose = require('mongoose');
+const validator = require('validator');
+
+const User = mongoose.model('User', {
+    name: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    password: {
+        type: String,
+        required: true,
+        minlength: 7,
+        trim: true,
+        validate(value) {
+            if(value.toLowerCase().includes('password')) {
+                throw new Error('Password cannot contain "password"')
+            }
+        }
+    },
+    email: {
+        type: String,
+        required: true,
+        trim: true,
+        lowercase: true,
+        validate(value) {   // validating using library
+            if(!validator.isEmail(value)) {
+                throw new Error('Email is invalid')
+            }
+        }
+    },
+    age: {
+        type: Number,
+        default: 0,
+        validate(value) {   // custom validating
+            if(value < 0) {
+                throw new Error('Age must be a positive number')
+            }
+        }
+    }
+});
+
+module.exports = User
