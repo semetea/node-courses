59312f1d59f444acfaf507f6ea81d05b400c8c50
semetea <taewooocean@gmail.com>
2022-05-02 12:59:31 -0400

    promise chaining:

--- playground/8-promises.js
+++ playground/8-promises.js
@@ -1,13 +1,43 @@
-const doWorkPromise = new Promise((resolve, reject) => {
-    setTimeout(() => {
-        // resolve([7,4,1])
-        reject('Things went wrong!!')
-        resolve([2, 3, 2])  // 중복되더라도 에러가 안 남
-    }, 2000)
-})
+// const doWorkPromise = new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//         // resolve([7,4,1])
+//         reject('Things went wrong!!')
+//         resolve([2, 3, 2])  // 중복되더라도 에러가 안 남
+//     }, 2000)
+// })
 
-doWorkPromise.then((result) => {    // goes well
-    console.log('Sucess!', result)
-}).catch((error) => {
-    console.log('Error!', error)
+// doWorkPromise.then((result) => {    // goes well
+//     console.log('Sucess!', result)
+// }).catch((error) => {
+//     console.log('Error!', error)
+// })
+
+const add = (a, b) => {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve(a + b)
+        }, 2000)
+    })
+}
+
+// add(1, 2).then((sum) => {   // too complex
+//     console.log(sum)
+
+//     add(sum, 5).then(sum2 => {
+//         console.log(sum2)
+//     }).catch(e => {
+//         console.log(e)
+//     })
+
+// }).catch(e => {
+//     console.log(e)
+// })
+
+add(1, 1).then((sum) => {   // Promise chaining
+    console.log(sum)
+    return add(sum, 4)
+}).then(sum2 => {
+    console.log(sum2)
+}).catch(e => {
+    console.log(e)
 })
