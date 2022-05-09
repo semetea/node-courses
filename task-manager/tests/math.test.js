97d441bb449c4bff71548e98b4e6687bd011efc6
semetea <taewooocean@gmail.com>
2022-05-09 12:03:13 -0400

    config

--- task-manager/tests/math.test.js
+++ task-manager/tests/math.test.js
@@ -1,4 +1,4 @@
-const { calculateTip, fahrenheitToCelsius, celsiusToFahrenheit } = require('../src/math')
+const { calculateTip, fahrenheitToCelsius, celsiusToFahrenheit, add } = require('../src/math')
 
 test('Should calculate total with tip', () => {
     const total = calculateTip(10, .3)
@@ -21,4 +21,23 @@ test('should convert 32 F to 0 C', () => {
 test('should convert 0 C to 32 F', () => {
     const temp = celsiusToFahrenheit(0)
     expect(temp).toBe(32)
+})
+
+// test('Async test demo', (done) => {
+//     setTimeout(() => {
+//         expect(1).toBe(2)
+//         done()
+//     }, 2000)     
+// })
+
+test('Should add two numbers', (done) => {
+    add(2,3).then((sum) => {
+        expect(sum).toBe(5)
+        done()
+    })
+})
+
+test('should add two numbers async/await', async () => {
+    const sum = await add(10,22)
+    expect(sum).toBe(32)
 })
