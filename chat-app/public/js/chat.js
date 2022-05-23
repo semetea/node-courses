02da4c4151930e4571f2f6ddb6c24540c363d7c6
semetea <55260856+semetea@users.noreply.github.com>
2022-05-23 15:24:39 -0400

    chat-app first commit

---
+++ chat-app/public/js/chat.js
@@ -0,0 +1,26 @@
+const socket = io()
+
+socket.on('message', (message) => {
+    console.log(message)
+})
+
+document.querySelector('#message-form').addEventListener('submit', (e) => {
+    e.preventDefault()
+
+    const message = e.target.elements.message.value
+
+    socket.emit('sendMessage', message)
+})
+
+document.querySelector('#send-location').addEventListener('click', () => {
+    if (!navigator.geolocation) {
+        return alert('Geolocation is not supported by your browser.')
+    }
+
+    navigator.geolocation.getCurrentPosition((position) => {
+        socket.emit('sendLocation', {
+            latitude: position.coords.latitude,
+            longtitude: position.coords.longitude
+        })
+    })
+})
--- chat-app/src/index.js
+++ chat-app/src/index.js
@@ -1,13 +1,37 @@
 const path = require('path')
+const http = require('http')
 const express = require('express')
+const socketio = require('socket.io')
 
 const app = express()
+const server = http.createServer(app)
+const io = socketio(server)
+
 const port = process.env.PORT || 3000
 
 const publicDirectoryPath = path.join(__dirname, '../public')
 
 app.use(express.static(publicDirectoryPath))
 
-app.listen(port, () => {
+io.on('connection', (socket) => {
+    console.log('New WebSocket connection')
+
+    socket.emit('message', 'Welcome')
+    socket.broadcast.emit('message', 'A new user has joined')
+
+    socket.on('sendMessage', (message) => {
+        io.emit('message', message)
+    })
+
+    socket.on('disconnect', () => {
+        io.emit('message', 'A user has left!')
+    })
+
+    socket.on('sendLocation', ({longtitude, latitude}) => {
+        io.emit('message', `https://google.com/maps?q=${latitude},${longtitude}`)
+    })
+})
+
+server.listen(port, () => {
     console.log('Server is up on port ' + port)
 })
